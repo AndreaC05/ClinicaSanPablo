80df75a85226ee9c027ff154f23aec5f8d0c90d1
Andrea Aguirre Ruiz <aguirreandrea457@gmail.com>
2024-09-04 17:55:15 -0500

    avance

---
+++ src/Components/Servicios.jsx
@@ -0,0 +1,66 @@
+import "../Style/Servicios.css";
+import PropTypes from "prop-types";
+import { Button } from "primereact/button";
+import Doctor from "../assets/Servicios/user-doctor-solid.png";
+import Users from "../assets/Servicios/users-solid.png";
+import Hospital from "../assets/Servicios/hospital-solid.png";
+import Staff from "../assets/Servicios/stethoscope-solid.png";
+
+function ServicioItem({ icono, titulo, descripcion, etiquetaBoton }) {
+  return (
+    <div className="section">
+      <div className="image_icon">
+        <img src={icono} alt={titulo} />
+      </div>
+      <div className="letters_serv">
+        <h3>{titulo}</h3>
+        <p>{descripcion}</p>
+      </div>
+      <div className="button_serv">
+        <Button label={etiquetaBoton} severity="warning" rounded />
+      </div>
+    </div>
+  );
+}
+
+// Definición de PropTypes para la validación de los props
+ServicioItem.propTypes = {
+  icono: PropTypes.string.isRequired,
+  titulo: PropTypes.string.isRequired,
+  descripcion: PropTypes.string.isRequired,
+  etiquetaBoton: PropTypes.string.isRequired,
+};
+
+export default function Servicios() {
+  return (
+    <>
+      <div className="container_servicios flex">
+        <ServicioItem
+          icono={Doctor}
+          titulo="Reserva una cita"
+          descripcion="Reserva una cita a través de nuestro canal digital"
+          etiquetaBoton="Reservar"
+        />
+        <ServicioItem
+          icono={Users}
+          titulo="Pacientes"
+          descripcion="Atención integral en tu bienestar y el de tu familia"
+          etiquetaBoton="Conoce más"
+        />
+        <ServicioItem
+          icono={Hospital}
+          titulo="Servicios"
+          descripcion="Conoce los servicio que brinda la Clínica San Pablo"
+          etiquetaBoton="Conoce más"
+        />
+
+        <ServicioItem
+          icono={Staff}
+          titulo="Staff Médico"
+          descripcion="Conoce a tu doctor de confianza y sus horarios"
+          etiquetaBoton="Conoce al Staff"
+        />
+      </div>
+    </>
+  );
+}
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import Navbar from "../Components/Navbar";
+import Servicios from "../Components/Servicios";
 
 export default function HomePage() {
     return(
         <>
             <div className="container_principal">
                 <Navbar />
+                <div className="servicios mt-5">
+                    <Servicios />
+                </div>
             </div>
         </>
     );
